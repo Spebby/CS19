1780a71f49e06367be903aa1b6fa6f3dee7aa90c
tomott (Thom Oliver Mott (He/Him/His)) <tomott@jeff.cis.cabrillo.edu>
2023-02-06 16:23:48 -0800

    attempt #2

--- as01/rosalind-again.cpp
+++ as01/rosalind-again.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 
-int main()
-{
+int main() {
     std::string input;
     std::cin >> input;
     float _density;
-    // get the percentage of C and G's in input
-    for (int i = 0; i < input.length(); i++)
-        if (input[i] == 'C' || input[i] == 'G')
-            _density++;
-    std::cout << (_density / input.length() * 100) << std::endl;
+    for (int i = 0; i < input.length(); i++) {
+        if (input[i] != 'A' && input[i] != 'T') {
+            if(input[i] == 'C' || input[i] == 'G') {
+                _density++;
+            }
+            else {
+                input.erase(i, 1);
+                i--;
+            }
+        }
+    }
+    std::printf("%.6f", (_density / input.length()) * 100);
     return 0;
 }
