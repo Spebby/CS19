7425d889b70e21afca490baeb074f080a3b936ad
tomott (Thom Oliver Mott (He/Him/His)) <tomott@jeff.cis.cabrillo.edu>
2023-02-13 15:40:01 -0800

    programming practice

--- as01/rosalind-again.cpp
+++ as01/rosalind-again.cpp
@@ -3,10 +3,11 @@
 
 int main() {
     float _gcDensity = 0, _atDensity = 0;
-    constexpr unsigned BUFFERSIZE = 8192;   
+    constexpr unsigned BUFFERSIZE = 8192;
     // no need to check eof, if gcount is less then buffer length, we're at the end.
     char buffer[BUFFERSIZE];
-    for (std::cin.read(buffer, BUFFERSIZE); std::cin.gcount() > 0; std::cin.read(buffer, BUFFERSIZE)) {
+    for (std::cin.read(buffer, BUFFERSIZE); std::cin.gcount() > 0;
+    std::cin.read(buffer, BUFFERSIZE)) {
         for (unsigned i = 0; i < std::cin.gcount(); i++) {
             switch (buffer[i]) {
                 case 'G':
