31a776d5721f6857fdf683a9ec01b9402012d5f4
tomott (Thom Oliver Mott (He/Him/His)) <tomott@jeff.cis.cabrillo.edu>
2023-02-12 20:43:59 -0800

    There's currently a bug with cin not recieving the full input. sars-covid 2 is at least a few thousand chars long, but cin only recieves 70 chars.

--- as01/rosalind-again.cpp
+++ as01/rosalind-again.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main() {
     std::string input;
-    std::cin >> input;
+    std::getline(std::cin, input);
     float _density = 0;
-    for (int i = 0; i < input.length(); i++) {
+    for (int i = 0; (unsigned)i < input.length(); i++) {
         if (input[i] == 'G' || input[i] == 'C') {
             _density++;
         } else if (input[i] != 'A' && input[i] != 'T') {
