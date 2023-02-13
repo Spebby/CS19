6a10ee9b6bc8dc26d31c453eecaaf95100d26c32
tomott (Thom Oliver Mott (He/Him/His)) <tomott@jeff.cis.cabrillo.edu>
2023-02-12 20:22:05 -0800

    attempt 5

--- as01/rosalind-again.cpp
+++ as01/rosalind-again.cpp
@@ -7,6 +7,9 @@ int main() {
     for (int i = 0; i < input.length(); i++) {
         if (input[i] == 'G' || input[i] == 'C') {
             _density++;
+        } else if (input[i] != 'A' && input[i] != 'T') {
+            input.erase(i, 1);
+            i--;
         }
     }
     std::cout << _density / input.length() * 100 << std::endl;
