4474a85652d5f07a973121b33c6b1171b0b31852
tomott (Thom Oliver Mott (He/Him/His)) <tomott@jeff.cis.cabrillo.edu>
2023-02-12 20:44:58 -0800

    i forgot to include this one too

--- as01/rosalind-again.cpp
+++ as01/rosalind-again.cpp
@@ -4,6 +4,7 @@
 int main() {
     std::string input;
     std::getline(std::cin, input);
+    //std::cout << input << ": " << input.length() << std::endl;
     float _density = 0;
     for (int i = 0; (unsigned)i < input.length(); i++) {
         if (input[i] == 'G' || input[i] == 'C') {
