a64d1c90e111134ca9b9a12056804563bb9f1f9c
tomott (Thom Oliver Mott (He/Him/His)) <tomott@jeff.cis.cabrillo.edu>
2023-04-04 18:41:49 -0700

    Assignment 7 - HSV, Update

--- as07/cs19_hsv_color.cpp
+++ as07/cs19_hsv_color.cpp
@@ -1,3 +1,75 @@
-//
-// Created by tomott on 4/3/23.
-//
+/**
+ * @author Thom Mott for CS 19, tomott@jeff.cis.cabrillo.edu
+ */
+
+#include <cs19_hsv_color.h>
+#include <string>
+#include <sstream>
+
+namespace cs19 {
+    class HsvColor {
+        public:
+
+
+        HsvColor(float hue, float saturation, float value) {
+            if (hue < 0 || hue > 360)
+                throw std::domain_error("Hue is not in domain [0, 360]");
+            if (saturation < 0 || saturation > 1)
+                throw std::domain_error("Saturation is not in domain [0, 1]");
+            if (value < 0 || value > 1)
+                throw std::domain_error("Value is not in domain [0, 1]");
+            
+            _hue = hue;
+            _saturation = saturation;
+            _value = value;
+        }
+
+        HsvColor operator~() const {
+            return HsvColor(_hue + 180, _saturation, _value);
+        }
+
+        HsvColor operator|(const HsvColor &that) const {
+            float hue = (_hue + that._hue) / 2;
+            float saturation = (_saturation + that._saturation) / 2;
+            float value = (_value + that._value) / 2;
+            return HsvColor(hue, saturation, value);
+        }
+
+        HsvColor grayscale() const {
+            return HsvColor(_hue, 0, _value);
+        }
+
+        std::string to_hex_string() const {
+            // convert RGB to hex
+            char rHex[3], gHex[3], bHex[3];
+            sprintf(rHex, "%X", _red); //convert number to hex
+            sprintf(gHex, "%X", _green);
+            sprintf(bHex, "%X", _blue);
+            std::string hexString = "#" + std::string(rHex)
+                + std::string(gHex) + std::string(bHex);
+            return hexString;
+        }
+
+        std::string to_rgb_string() const {
+            return "rgb(" + std::to_string(_red) + ", " + std::to_string(_green) +
+                    ", " + std::to_string(_blue) + ")";
+        }
+
+        // i miss '=>' (expression-bodied members)
+        float hue() const { return _hue; }
+        float saturation() const { return _saturation; }
+        float value() const { return _value; }
+        
+        int red() const { return _red; }
+        int green() const { return _green; }
+        int blue() const { return _blue; }
+
+        private:
+        float _hue;  // [0, 360]
+        float _saturation;  // [0, 1]
+        float _value;  // [0, 1]
+        float _red;  // [0, 255]
+        float _green;  // [0, 255]
+        float _blue;  // [0, 255]
+    };
+}  // namespace CS19
--- as07/cs19_hsv_color.h
+++ as07/cs19_hsv_color.h
@@ -1,8 +1,64 @@
-//
-// Created by tomott on 4/3/23.
-//
+/**
+ * @author Thom Mott for CS 19, tomott@jeff.cis.cabrillo.edu
+ */
 
-#ifndef CS19S23_CS19_HSV_COLOR_H
-#define CS19S23_CS19_HSV_COLOR_H
 
-#endif //CS19S23_CS19_HSV_COLOR_H
+#ifndef CS19_HSV_COLOR_H_
+#define CS19_HSV_COLOR_H_
+
+#include <string>
+
+namespace cs19 {
+    class HsvColor {
+        public:
+        /**
+         * Constructs a new HsvColor with the given HSV components
+         * @param hue the hue component of the color
+         * @param saturation the saturation component of the color
+         * @param value the value component of the color
+         * @throw std::domain_error if any component is out of range.
+         */
+        HsvColor(float hue, float saturation, float value);
+
+        /**
+         * @return the complementary color, with the hue shifted 180 degrees.
+         */
+        HsvColor operator~() const;
+        /**
+         * @param that the color to the right of the operator; the comparison.
+         * @return a color that is linerly interpolated between this color and another.
+         */
+        HsvColor operator|(const HsvColor &that) const;
+
+        /**
+         * @return the greyscale (desaturated) version of this color.
+         */
+        HsvColor grayscale() const;
+
+        /**
+         * @return Returns a CSS-compatible color string in 24-bit RGB hexadecimal format, e.g.
+         * "#000000" for black and "#6495ed" for cornflower blue.
+         *
+         * @see https://www.w3schools.com/cssref/css_colors_legal.php
+         */
+        std::string to_hex_string() const;
+
+        /**
+         * @return Returns a CSS-compatible color string in 24-bit RGB decimal format, e.g.
+         * "rgb(0,0,0)" for black and "rgb(100, 149, 237)" for cornflower blue.
+         *
+         * @see https://www.w3schools.com/cssref/css_colors_legal.php
+         */
+        std::string to_rgb_string() const;
+
+        // may or may not actually exist?
+        float hue() const;
+        float saturation() const;
+        float value() const;
+        int red() const;
+        int green() const;
+        int blue() const;
+    };
+}  // namespace cs19
+
+#endif  // CS19_HSV_COLOR_H_
